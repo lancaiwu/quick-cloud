72bee01701be8ce7ed6214114258e230e707482e
lancaiwu <lancaiwu@gmail.com>
2020-03-31 14:12:38 +0800

    gateway调整

--- api-gateway/src/main/java/com/lancaiwu/cloud/apigateway/config/RedisRateLimiterConfig.java
+++ api-gateway/src/main/java/com/lancaiwu/cloud/apigateway/config/RedisRateLimiterConfig.java
@@ -1,3 +1,4 @@
+/*
 package com.lancaiwu.cloud.apigateway.config;
 
 import org.springframework.cloud.gateway.filter.ratelimit.KeyResolver;
@@ -5,6 +6,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import reactor.core.publisher.Mono;
 
+*/
 /**
  * Created with IntelliJ IDEA.
  * User: lancaiwu
@@ -17,26 +19,32 @@ import reactor.core.publisher.Mono;
  * 两种策略
  * 1、一种是根据请求参数中的username进行限流，
  * 2、一种是根据访问IP进行限流
- */
+ *//*
+
 @Configuration
 public class RedisRateLimiterConfig {
-    /**
+    */
+/**
      * 根据请求参数中的username进行限流
      *
      * @return
-     */
+     *//*
+
 //    @Bean(value = "userKeyResolver")
 //    public KeyResolver userKeyResolver() {
 //        return exchange -> Mono.just(exchange.getRequest().getQueryParams().getFirst("username"));
 //    }
 
-    /**
+    */
+/**
      * 根据访问ip进行限流
      *
      * @return
-     */
+     *//*
+
     @Bean(value = "ipKeyResolver")
     public KeyResolver ipKeyResolver() {
         return exchange -> Mono.just(exchange.getRequest().getRemoteAddress().getHostName());
     }
 }
+*/
